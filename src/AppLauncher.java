91968d156466c9dd75c14f254ad5271e2a3d6acb
watchfoxie <tocana.mar@gmail.com>
2025-05-10 16:25:22 +0300

    app style customization using nimbus look and feel theme from swing

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,21 +1,19 @@
-import db_objs.User;
-import guis.BankingAppGui;
 import guis.LoginGui;
-import guis.RegisterGui;
-
 import javax.swing.*;
-import java.math.BigDecimal;
 
 public class AppLauncher {
     public static void main(String[] args) {
-        // Utilizați invokeLater pentru a face actualizările la GUI mai sigure pentru fire
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new LoginGui().setVisible(true);
-//                new RegisterGui().setVisible(true);
-//                new BankingAppGui(new User(1,"username", "password", new BigDecimal("34.00"))).setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (Exception e) {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             }
+            new LoginGui().setVisible(true);
         });
     }
 }
